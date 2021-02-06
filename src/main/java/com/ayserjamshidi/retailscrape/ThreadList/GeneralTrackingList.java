7a90afb8bb8e7290ffda7b925e97da2259441837
ayserjamshidi <acejamshidi4th@gmail.com>
2021-02-05 21:27:18 -0500

    Added more URL filtering options to BestBuyListUSA 3090, 3080, 3070, 6900XT, 5950X, 5900X and 5800X.

--- src/main/java/com/ayserjamshidi/retailscrape/ThreadList/GeneralTrackingList.java
+++ src/main/java/com/ayserjamshidi/retailscrape/ThreadList/GeneralTrackingList.java
@@ -130,13 +130,13 @@ class BestBuyListUSA {
 	public Thread[] threads = {
 			// Nvidia GPU
 			new BestBuySearch("Nvidia 3090 - BestBuy USA", DiscordChannel.NVIDIA_3090_USA, new String[]{
-					"https://www.bestbuy.com/site/searchpage.jsp?_dyncharset=UTF-8&id=pcat17071&iht=y&keys=keys&ks=960&list=n&qp=category_facet%3DGPUs%20%2F%20Video%20Graphics%20Cards~abcat0507002&sc=Global&st=rtx%203090&type=page&usc=All%20Categories"
+					"https://www.bestbuy.com/site/searchpage.jsp?_dyncharset=UTF-8&id=pcat17071&iht=y&keys=keys&ks=960&list=n&qp=category_facet%3DGPUs%20%2F%20Video%20Graphics%20Cards~abcat0507002&sc=Global&st=rtx%203090%20gpu&type=page&usc=All%20Categories"
 			}),
 			new BestBuySearch("Nvidia 3080 - BestBuy USA", DiscordChannel.NVIDIA_3080_USA, new String[]{
-					"https://www.bestbuy.com/site/searchpage.jsp?_dyncharset=UTF-8&id=pcat17071&iht=y&keys=keys&ks=960&list=n&qp=category_facet%3DGPUs%20%2F%20Video%20Graphics%20Cards~abcat0507002&sc=Global&st=rtx%203080&type=page&usc=All%20Categories"
+					"https://www.bestbuy.com/site/searchpage.jsp?_dyncharset=UTF-8&id=pcat17071&iht=y&keys=keys&ks=960&list=n&qp=category_facet%3DGPUs%20%2F%20Video%20Graphics%20Cards~abcat0507002&sc=Global&st=rtx%203080%20gpu&type=page&usc=All%20Categories"
 			}),
 			new BestBuySearch("Nvidia 3070 - BestBuy USA", DiscordChannel.NVIDIA_3070_USA, new String[]{
-					"https://www.bestbuy.com/site/searchpage.jsp?_dyncharset=UTF-8&id=pcat17071&iht=y&keys=keys&ks=960&list=n&qp=category_facet%3DGPUs%20%2F%20Video%20Graphics%20Cards~abcat0507002&sc=Global&st=rtx%203070&type=page&usc=All%20Categories"
+					"https://www.bestbuy.com/site/searchpage.jsp?_dyncharset=UTF-8&id=pcat17071&iht=y&keys=keys&ks=960&list=n&qp=category_facet%3DGPUs%20%2F%20Video%20Graphics%20Cards~abcat0507002&sc=Global&st=rtx%203070%20gpu&type=page&usc=All%20Categories"
 			}),
 			new BestBuySearch("Nvidia 3060ti - BestBuy USA", DiscordChannel.NVIDIA_3060TI_USA, new String[]{
 					"https://www.bestbuy.com/site/searchpage.jsp?_dyncharset=UTF-8&id=pcat17071&iht=y&keys=keys&ks=960&list=n&qp=category_facet%3DGPUs%20%2F%20Video%20Graphics%20Cards~abcat0507002&sc=Global&st=rtx%203060%20ti&type=page&usc=All%20Categories"
@@ -144,7 +144,7 @@ class BestBuyListUSA {
 
 			// AMD GPU
 			new BestBuySearch("AMD 6900XT - BestBuy USA", DiscordChannel.AMD_RX6900XT_USA, new String[]{
-					"https://www.bestbuy.com/site/searchpage.jsp?st=amd+6900+gpu&_dyncharset=UTF-8&_dynSessConf=&id=pcat17071&type=page&sc=Global&cp=1&nrp=&sp=&qp=&list=n&af=true&iht=y&usc=All+Categories&ks=960&keys=keys"
+					"https://www.bestbuy.com/site/searchpage.jsp?_dyncharset=UTF-8&id=pcat17071&iht=y&keys=keys&ks=960&list=n&qp=category_facet%3DGPUs%20%2F%20Video%20Graphics%20Cards~abcat0507002&sc=Global&st=amd%206900%20gpu&type=page&usc=All%20Categories"
 			}),
 			new BestBuySearch("AMD 6800XT - BestBuy USA", DiscordChannel.AMD_RX6800XT_USA, new String[]{
 					"https://www.bestbuy.com/site/searchpage.jsp?_dyncharset=UTF-8&id=pcat17071&iht=y&keys=keys&ks=960&list=n&qp=category_facet%3DGPUs%20%2F%20Video%20Graphics%20Cards~abcat0507002%5Egpusv_facet%3DGraphics%20Processing%20Unit%20(GPU)~AMD%20Radeon%20RX%206800%20XT&sc=Global&st=amd%206800&type=page&usc=All%20Categories"
@@ -155,13 +155,13 @@ class BestBuyListUSA {
 
 			// CPU
 			new BestBuySearch("AMD 5950X - BestBuy USA", DiscordChannel.AMD_5950x_USA, new String[]{
-					"https://www.bestbuy.com/site/searchpage.jsp?st=5950x&_dyncharset=UTF-8&_dynSessConf=&id=pcat17071&type=page&sc=Global&cp=1&nrp=&sp=&qp=&list=n&af=true&iht=y&usc=All+Categories&ks=960&keys=keys"
+					"https://www.bestbuy.com/site/searchpage.jsp?_dyncharset=UTF-8&id=pcat17071&iht=y&keys=keys&ks=960&list=n&qp=category_facet%3DCPUs%20%2F%20Processors~abcat0507010&sc=Global&st=5950x&type=page&usc=All%20Categories"
 			}),
 			new BestBuySearch("AMD 5900X - BestBuy USA", DiscordChannel.AMD_5900x_USA, new String[]{
-					"https://www.bestbuy.com/site/searchpage.jsp?st=5900x&_dyncharset=UTF-8&_dynSessConf=&id=pcat17071&type=page&sc=Global&cp=1&nrp=&sp=&qp=&list=n&af=true&iht=y&usc=All+Categories&ks=960&keys=keys"
+					"https://www.bestbuy.com/site/searchpage.jsp?_dyncharset=UTF-8&id=pcat17071&iht=y&keys=keys&ks=960&list=n&qp=category_facet%3DCPUs%20%2F%20Processors~abcat0507010&sc=Global&st=5900x&type=page&usc=All%20Categories"
 			}),
 			new BestBuySearch("AMD 5800X - BestBuy USA", DiscordChannel.AMD_5800x_USA, new String[]{
-					"https://www.bestbuy.com/site/searchpage.jsp?st=5800x&_dyncharset=UTF-8&_dynSessConf=&id=pcat17071&type=page&sc=Global&cp=1&nrp=&sp=&qp=&list=n&af=true&iht=y&usc=All+Categories&ks=960&keys=keys"
+					"https://www.bestbuy.com/site/searchpage.jsp?_dyncharset=UTF-8&id=pcat17071&iht=y&keys=keys&ks=960&list=n&qp=category_facet%3DCPUs%20%2F%20Processors~abcat0507010&sc=Global&st=5800x&type=page&usc=All%20Categories"
 			}),
 			new BestBuySearch("AMD 5600X - BestBuy USA", DiscordChannel.AMD_5600x_USA, new String[]{
 					"https://www.bestbuy.com/site/searchpage.jsp?_dyncharset=UTF-8&id=pcat17071&iht=y&keys=keys&ks=960&list=n&qp=category_facet%3DCPUs%20%2F%20Processors~abcat0507010&sc=Global&st=5600x&type=page&usc=All%20Categories"
